b5498c9909cb3af7ab94e21b5755bb115845ff9a
a012 <Aeagan0507@gmail.com>
2025-12-02 16:50:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+namespace std {
+    export import __math = Math 
+
+    export enum __corePropertyMethods {
+     // expose them here
+    }  
+
+    export function __propertyCore(core: __corePropertyMethods) {
+        return core;
+    }
+
+    // create more here
+} 
